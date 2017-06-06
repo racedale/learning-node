0f98fe1ebf4a9c3d348de2f1188bdbeeb56667fe
Dale Race <dale.race@daugherty.com>
2017-06-05 21:12:38 -0500

    Add comma to fix 'success is not a function' error

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -36,6 +36,6 @@ exports.updateStore = async (req, res) => {
     runValidators: true
   }).exec();
   // 2. redirect them to the store and tell them it worked
-  req.flash('success' `Successfully updated ${store.name} <a href="/stores/${store.slug}">View Store</a>`);
+  req.flash('success', `Successfully updated ${store.name} <a href="/stores/${store.slug}">View Store</a>`);
   res.redirect(`/stores/${store._id}/edit`);
 }
