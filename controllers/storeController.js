7e36095a601323abad5366beb29a64c5c75d7ce8
Dale Race <dale.race@daugherty.com>
2017-06-05 20:59:07 -0500

    Update to 15

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -14,3 +14,28 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 }
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores })
+}
+
+exports.editStore = async (req, res) => {
+  // 1. Find store given the id
+  // 2. confirm they are the owner of the store
+  // 3. Render the edit form so user can update their store
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', { title: `Edit ${store.name}`, store: store })
+}
+
+exports.updateStore = async (req, res) => {
+  // 1. find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  // 2. redirect them to the store and tell them it worked
+  req.flash('success' `Successfully updated ${store.name} <a href="/stores/${store.slug}">View Store</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+}
