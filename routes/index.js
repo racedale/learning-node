e59b1061b3c1fc08d46c376ecf24b5bc0f921bb1
Dale Race <dale.race@daugherty.com>
2017-06-15 21:12:48 -0500

    Create login and register pages, and validate user input on submit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -25,4 +26,7 @@ router.post('/add/:id', catchErrors(storeController.updateStore));
 router.get('/tags', catchErrors(storeController.getStoresbyTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresbyTag));
 
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+router.post('/register', userController.validateRegister);
 module.exports = router;
