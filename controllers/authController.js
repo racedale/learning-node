ca087bd07cdee2acc209a67d57caefc1c72c436d
Dale Race <dale.race@daugherty.com>
2017-07-05 22:34:00 -0500

    Create a route guard for the add form

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,13 @@ exports.logout = (req, res) => {
   req.flash('success', 'You are now logged out!');
   res.redirect('/');
 }
+
+exports.isLoggedIn = (req, res, next) => {
+  // first check if the user is authenticated
+  if (req.isAuthenticated()) { // isAuthenticated comes from the passport plugin!
+    next(); // carry on! they are logged in
+    return;
+  }
+  req.flash('error', 'Oops, you must be logged in to do that!');
+  res.redirect('/login');
+}
--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/stores', catchErrors(storeController.getStores));
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 router.post('/add',
   storeController.upload,
   catchErrors(storeController.resize),
