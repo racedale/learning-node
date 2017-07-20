21da8ea283044b8628af6e7c4a46fb1f0a5267e0
Race, Dale <Dale.Race@mastercard.com>
2017-07-20 15:26:39 -0500

    Update funny console emoji messages

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Make sure we are running node 7.6+
 const [major, minor] = process.versions.node.split('.').map(parseFloat);
 if (major < 7 || (major === 7 && minor <= 5)) {
-  console.log('🛑 🌮 🐶 💪 💩\nHey You! \n\t ya you! \n\t\tBuster! \n\tYou\'re on an older version of node that doesn\'t support the latest and greatest things we are learning (Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n ');
+  console.log('🛑 💩\nHey You! \n\t ya you! \n\tYou\'re on an older version of node that doesn\'t support the latest and greatest things(Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n ');
   process.exit();
 }
 
@@ -14,7 +14,7 @@ require('dotenv').config({ path: 'variables.env' });
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
-  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+  console.error(`🛑 🚫 🛑 🚫 🛑 🚫 🛑 🚫 → ${err.message}`);
 });
 
 // READY?! Let's go!
