2d33b15f2cb78a99a35cb94a9f2b73d3ed3106e2
Race, Dale <Dale.Race@mastercard.com>
2017-10-18 15:00:25 -0500

    Index maps and return which ones are nearest a coordinate set

--- routes/index.js
+++ routes/index.js
@@ -54,5 +54,6 @@ router.post('/account/reset/:token',
 */
 
 router.get('/api/v1/search', catchErrors(storeController.searchStores))
+router.get('/api/v1/stores/near', catchErrors(storeController.mapStores))
 
 module.exports = router;
