65a5bf85c8dbe520c5ec88ae236534ae28005e6e
Race, Dale <Dale.Race@mastercard.com>
2017-11-01 11:16:27 -0500

    Add review saving

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -50,7 +51,8 @@ router.post('/account/reset/:token',
 );
 
 router.get('/map', storeController.mapPage);
-
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts))
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview))
 /*
   API
 */
@@ -58,6 +60,5 @@ router.get('/map', storeController.mapPage);
 router.get('/api/v1/search', catchErrors(storeController.searchStores))
 router.get('/api/v1/stores/near', catchErrors(storeController.mapStores))
 router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore))
-router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts))
 
 module.exports = router;
